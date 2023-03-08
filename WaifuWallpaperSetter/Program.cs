d4392227d4388da1d990cf21ddc058488125ce6b
SoNearSonar <sonearsonar@gmail.com>
2023-03-08 09:53:21 -0500

    Add files for current state of the project

---
+++ WaifuWallpaperSetter/Program.cs
@@ -0,0 +1,48 @@
+﻿using WaifuImAPI_NET;
+using WaifuImAPI_NET.Models.Enums;
+using WaifuImAPI_NET.Models.Objects;
+using WaifuImAPI_NET.Models.Objects.Lists;
+
+bool isNotWindowsPlatform = Environment.OSVersion.Platform != PlatformID.Win32NT;
+bool isNotCompatibleWindowsPlatform = Environment.OSVersion.Version.Major + Environment.OSVersion.Version.Minor < 7;
+
+if (isNotWindowsPlatform || isNotCompatibleWindowsPlatform)
+{
+    Console.WriteLine("You can only run this program on Windows 7 or later");
+    Console.WriteLine("Press any key to exit.");
+    Console.ReadKey();
+    Environment.Exit(0);
+}
+
+WaifuImClient client = new WaifuImClient();
+WaifuImSearchSettings settings = new WaifuImSearchSettings()
+{
+    Orientation = Orientation.Landscape
+};
+
+try
+{
+    Console.WriteLine("Getting wallpaper from Waifu.Im\n");
+    WaifuImImageList imageList = await client.GetImagesAsync(settings);
+    WaifuImImage image = imageList.Images[0];
+
+    string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "image.png");
+    Console.WriteLine($"Downloading wallpaper from Waifu.Im with image URL of \"{image.Url}\" to \"{path}\"\n");
+    HttpClient httpClient = new HttpClient();
+    byte[] wallpaperImage = await httpClient.GetByteArrayAsync(image.Url);
+    await File.WriteAllBytesAsync(path, wallpaperImage);
+
+    Console.WriteLine($"Setting wallpaper to image from \"{path}\"\n");
+    Wallpaper.Set(path, Wallpaper.Style.Fill);
+
+    Console.WriteLine($"Done setting wallpaper!\n");
+    Console.WriteLine("Press any key to exit.");
+    Console.ReadKey();
+}
+catch (Exception ex)
+{
+    Console.WriteLine("An error occurred with this program: " + ex.Message);
+    Console.WriteLine("Press any key to exit.");
+    Console.ReadKey();
+    Environment.Exit(0);
+}
---
+++ WaifuWallpaperSetter/Wallpaper.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Win32;
+using System.Runtime.InteropServices;
+
+
+/*
+ * Credit to: https://stackoverflow.com/users/4792350/badsamaritan
+ * From: https://stackoverflow.com/a/40950312
+ */
+public sealed class Wallpaper
+{
+    Wallpaper() { }
+
+    const int SPI_SETDESKWALLPAPER = 20;
+    const int SPIF_UPDATEINIFILE = 0x01;
+    const int SPIF_SENDWININICHANGE = 0x02;
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+    public enum Style : int
+    {
+        Fill,
+        Fit,
+        Span,
+        Stretch,
+        Tile,
+        Center
+    }
+
+    public static void Set(string imagePath, Style style)
+    {
+        RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+        if (style == Style.Fill)
+        {
+            key.SetValue(@"WallpaperStyle", "10");
+            key.SetValue(@"TileWallpaper", "0");
+        }
+        if (style == Style.Fit)
+        {
+            key.SetValue(@"WallpaperStyle", "6");
+            key.SetValue(@"TileWallpaper", "0");
+        }
+        if (style == Style.Span) // Windows 8 or newer only!
+        {
+            key.SetValue(@"WallpaperStyle", "22");
+            key.SetValue(@"TileWallpaper", "0");
+        }
+        if (style == Style.Stretch)
+        {
+            key.SetValue(@"WallpaperStyle", "2");
+            key.SetValue(@"TileWallpaper", "0");
+        }
+        if (style == Style.Tile)
+        {
+            key.SetValue(@"WallpaperStyle", "0");
+            key.SetValue(@"TileWallpaper", "1");
+        }
+        if (style == Style.Center)
+        {
+            key.SetValue(@"WallpaperStyle", "0");
+            key.SetValue(@"TileWallpaper", "0");
+        }
+
+        SystemParametersInfo(SPI_SETDESKWALLPAPER,
+            0,
+            imagePath,
+            SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+    }
+}
